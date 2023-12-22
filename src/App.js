746e1e0ae1cfa52efdd70f89a8d9a38a2e02cccd
maseelshah22 <98069253+maseelshah22@users.noreply.github.com>
2023-12-22 17:12:08 -0500

    made template for skill bars

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Navbar from "./components/NavBar/navbar.js";
 import Intro from "./components/Intro/intro.js";
+import Skills from "./components/Skills/skills.js";
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Intro/>
+    <Skills/>
+    
     </div>
+    
   );
 }
 
