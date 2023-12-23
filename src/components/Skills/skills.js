8fff353c8e68caaa1484e3d2e9b269bfe00b637a
maseelshah22 <98069253+maseelshah22@users.noreply.github.com>
2023-12-23 00:36:54 -0500

    finished my skills template section

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -7,15 +7,33 @@ import AppDesign from "../../assets/app-design.png";
 const Skills = () => {
   return (
     <section id="skills">
-      <span className="skillTitle">What I do</span>
-      <span className="skillDesc">I am a skilled...</span>
+      <span className="skillTitle">What I do </span>
+      <span className="skillDesc">
+        I am a skilled
+        lajflajf;ajfl;ajlja;jfa;fja;jfda;jfa;jfd;ajfajf;aj;ajfa;jf;aj;ja hahahah
+        ok fill in .
+      </span>
       <div className="skillBars">
         <div className="skillBar">
-            <img src="" alt="" className="skillBarImg" />
-            <div className="skillBarText">
-                <h2></h2>
-                <p></p>
-            </div>
+          <img src={UIDesign} alt="UIDesign" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>UI/UX Design</h2>
+            <p>This is a demo text</p>
+          </div>
+        </div>
+        <div className="skillBar">
+          <img src={WebDesign} alt="WebDesign" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>Web Design</h2>
+            <p>This is a demo text</p>
+          </div>
+        </div>
+        <div className="skillBar">
+          <img src={AppDesign} alt="AppDesign" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>App Design</h2>
+            <p>This is a demo text</p>
+          </div>
         </div>
       </div>
     </section>
